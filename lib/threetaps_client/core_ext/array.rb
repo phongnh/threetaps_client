aa71e597741cca201c76ffe21c782c7f6f28a7e6
Phong Nguyen <nhphong1406@gmail.com>
2014-03-12 16:33:08 +0700

    Fix return error when array.rb is loaded

--- lib/threetaps_client/core_ext/array.rb
+++ lib/threetaps_client/core_ext/array.rb
@@ -1,7 +1,7 @@
-return if {}.respond_to?(:extract_options!)
-
-class Array
-  def extract_options!
-    self.last.is_a?(Hash) ? self.pop : {}
+unless {}.respond_to?(:extract_options!)
+  class Array
+    def extract_options!
+      self.last.is_a?(Hash) ? self.pop : {}
+    end
   end
 end
